edc3c7cb7b37f0f1391a7d0e14c67becfb311c6d
must <mustafa.tahir@etraveli.com>
2013-12-04 00:34:40 +0100

    - cleaning up tests - added codeStyleSettings.xml for intellij

--- test/org/jetbrains/testnames/NameSuggestionsProviderTest.java
+++ test/org/jetbrains/testnames/NameSuggestionsProviderTest.java
@@ -1,6 +1,7 @@
 package org.jetbrains.testnames;
 
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.junit.Test;
@@ -16,17 +17,22 @@ public class NameSuggestionsProviderTest {
 
   @Test
   public void Should_Return_Null_List_When_No_Initial_Name_Exists() throws Exception {
-    assertNull(new NameSuggestionsProvider(null).get());
+    assertNull(suggestionsFor(null));
   }
 
   @Test
   public void Should_Return_A_List_Of_Size_One_When_Initial_Name_Is_Provided() throws Exception {
-    safeAssertEquals("Some_Camel_Case_Initial_Text", new NameSuggestionsProvider("SomeCamelCaseInitialText").get());
+    safeAssertEquals("Some_Camel_Case_Initial_Text", suggestionsFor("SomeCamelCaseInitialText"));
+  }
+
+  @Test
+  public void Should_Return_The_Same_String_If_It_Is_Only_Test() throws Exception {
+    safeAssertEquals("test", suggestionsFor("test"));
   }
 
   @Test
   public void Should_Add_One_More_Suggestion_That_Removes_The_Test_Prefix_Of_The_Name() throws Exception {
-    Set<String> suggestions = new NameSuggestionsProvider("testSomeCamelCaseInitialText").get();
+    Set<String> suggestions = suggestionsFor("testSomeCamelCaseInitialText");
     assertNotNull(suggestions);
 
     Iterator<String> iterator = suggestions.iterator();
@@ -35,14 +41,13 @@ public class NameSuggestionsProviderTest {
     assertEquals("test_Some_Camel_Case_Initial_Text", iterator.next());
   }
 
-  @Test
-  public void Should_Return_The_Same_String_If_It_Is_Only_Test() throws Exception {
-    safeAssertEquals("test", new NameSuggestionsProvider("test").get());
-  }
-
   private void safeAssertEquals(String expected, Set<String> suggestions) {
     assertNotNull(suggestions);
     assertEquals(1, suggestions.size());
     assertEquals(expected, suggestions.iterator().next());
+  }
+
+  private LinkedHashSet<String> suggestionsFor(String initialText) {
+    return new NameSuggestionsProvider(initialText).get();
   }
 }
