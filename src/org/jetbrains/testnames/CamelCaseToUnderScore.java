8baa846cb1799dafcbd83c88713899d155a432a2
must <must.tahir@fourpoint.se>
2015-02-21 18:14:38 +0100

    Applying the refactoring twice adds double underscore
    Resolves: issue#2

--- src/org/jetbrains/testnames/CamelCaseToUnderScore.java
+++ src/org/jetbrains/testnames/CamelCaseToUnderScore.java
@@ -5,13 +5,22 @@ final class CamelCaseToUnderScore {
   private CamelCaseToUnderScore() {
   }
 
-  static String transform(String stringToTransform) {
-    return stringToTransform.replaceAll(
+  static String transform(String str) {
+    String transformed = camelCaseToUnderscore(str);
+    return allowOnlySingleUnderscore(transformed);
+  }
+
+  private static String camelCaseToUnderscore(String str) {
+    return str.replaceAll(
       String.format("%s|%s|%s",
                     "(?<=[A-Z])(?=[A-Z][a-z])",
                     "(?<=[^A-Z])(?=[A-Z])",
                     "(?<=[A-Za-z])(?=[^A-Za-z])"
       ),
       "_");
+  }
+
+  private static String allowOnlySingleUnderscore(String str) {
+    return str.replaceAll(String.format("%s", "\\_+"), "_");
   }
 }
--- test/org/jetbrains/testnames/CamelCaseToUnderScoreTest.java
+++ test/org/jetbrains/testnames/CamelCaseToUnderScoreTest.java
@@ -16,6 +16,16 @@ public class CamelCaseToUnderScoreTest {
     assertEqualsAfterTransformed("testConvert2Map", "test_Convert_2_Map");
   }
 
+  @Test
+  public void Do_Not_Add_More_Underscores_If_Already_Transformed() {
+    assertEqualsAfterTransformed("a__a", "a_a");
+    assertEqualsAfterTransformed("a___a", "a_a");
+    assertEqualsAfterTransformed("__a___a", "_a_a");
+    assertEqualsAfterTransformed("a___b_________c", "a_b_c");
+    assertEqualsAfterTransformed("When_Exception_Is_Thrown_During_Command_Creation_Then_Perform_Should_Throw_Exception",
+                                 "When_Exception_Is_Thrown_During_Command_Creation_Then_Perform_Should_Throw_Exception");
+  }
+
   private void assertEqualsAfterTransformed(String actualToTransform, String expected) {
     assertEquals(expected, CamelCaseToUnderScore.transform(actualToTransform));
   }
